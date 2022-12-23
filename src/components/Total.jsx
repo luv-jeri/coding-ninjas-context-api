ed2ab305ef11282a3cdda0e71fb153022d355369
luv-jeri <luvvjeri@gmail.com>
2022-12-23 15:06:02 +0530

    =

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import styles from '../styles/Total.module.css';
-import { useContext } from 'react';
-import { context } from '../context';
 
 function Total() {
-  const { total } = useContext(context);
   return (
     <div className={styles.container}>
-      <h1>Total - {total}</h1>
+      <h1>Total - 0</h1>
       <h1> Item - 0 </h1>
     </div>
   );
